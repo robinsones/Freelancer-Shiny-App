a82bda30ba0f186f8093c1df468cf51ed5611712
Emily Robinson <robinson.es@gmail.com>
2016-09-12 21:06:02 -0400

    current working version

--- fix_jobs.R
+++ fix_jobs.R
@@ -1,11 +1,5 @@
-library(dplyr)
-
-library(readr)
-jobs <- read_csv("~/Dropbox/Final_Project_Shiny_App/jobs.csv")
-
-jobs <- jobs %>%
-  mutate(skills_fixed = (gsub("\\[|\\]|'", "", skills))) %>%
-  mutate(skills_fixed = as.list(strsplit(skills_fixed, ",")))
+library(tidyverse)
+library(lsa)
 
 job_skill_count <- jobs %>%
   select(skills_fixed, id)
@@ -19,13 +13,78 @@ job_skill_count <- job_skill_count %>%
   filter(n >= 10)
 
 write.csv(job_skill_count, "popular_job_skills.csv")
-popular_job_skills.csv <- job_skill_count
+popular_job_skills <- job_skill_count
 
 unique(unlist(jobs$skills_fixed))
 
 # remove quotation marks and brackets from skills column 
 kk <- gsub("\\[|\\]", "", jobs$skills[1])
 kk <- gsub("'", "", kk)
+
+
+library(tm)
+library(tidytext)
+library(janeaustenr)
+
+job_words <- jobs %>%
+  select(snippet, id)
+
+job_words <- job_words %>%
+  unnest_tokens(word, snippet) %>%
+  count(id, word, sort = TRUE) %>%
+  ungroup()
+
+# select words appearing less than 6 times
+rare_words <- job_words %>%
+  count(word) %>%
+  filter(nn < 6)
+
+word_count
+
+total_words <- job_words %>% group_by(id) %>% summarize(total = sum(n))
+job_words <- left_join(job_words, total_words)
+job_words
+
+data("stop_words")
+job_words <- job_words %>%
+  anti_join(stop_words) %>%
+  anti_join(rare_words)
+
+job_words <- job_words %>%
+  bind_tf_idf(word, id, n)
+
+# now job words has tf_idf, word, and id. We need to spread
+
+job_words
+
+tfidf_vector <- job_words %>%
+  select(word, id, tf_idf) %>%
+  spread(word, tf_idf)
+
+tfidf_vector[is.na(tfidf_vector)] <- 0
+
+cos.sim <- function(A, B) 
+{
+  return( sum(A*B)/sqrt(sum(A^2)*sum(B^2)) )
+}   
+
+# make into vectors and get rid of NAs
+tfidfv_ex = as.numeric(tfidf_vector[2,])
+tfidfv_ex1 = as.numeric(tfidf_vector[20,])
+
+tfidfv_ex1[is.na(tfidfv_ex1)] <- 0
+tfidfv_ex[is.na(tfidfv_ex)] <- 0
+
+lsa::cosine(tfidfv_ex, tfidfv_ex1)
+
+
+corpus <- tm_map(jobs$snippet, removeWords, stopwords("english"))
+
+corpus <- tm_map(corpus, stripWhitespace)
+corpus <- tm_map(corpus, stemDocument, language="english")
+#creating term matrix with TF-IDF weighting
+terms <-DocumentTermMatrix(corpus,control = list(weighting = function(x) weightTfIdf(x, normalize = FALSE)))
+
 
 as.list(strsplit(jobs$skills[3], ",")[[1]])
         
@@ -35,3 +94,48 @@ jobs$skills[1]
 
 jobs %>%
   mutate(skills_fixed = as.list(strsplit(skills, ",")[[1]]))
+
+cosine_similarities <- function(profile_text, jobs){
+  # add profile text to job words
+  job_words <- jobs %>%
+    select(snippet, id) %>%
+    add_row(snippet = profile_text, id = "profile_id", .before = 1) %>% 
+    unnest_tokens(word, snippet) %>% count(id, word, sort = TRUE) %>% ungroup()
+# select words appearing less than 6 times and no stop words
+  rare_words <- job_words %>% count(word) %>% filter(nn < 6)
+  total_words <- job_words %>%  group_by(id) %>%summarize(total = sum(n))
+  job_words <- left_join(job_words, total_words)
+  data("stop_words")
+  job_words <- job_words %>% anti_join(stop_words) %>% anti_join(rare_words)
+# get tfidf vectors
+  tfidf_vector <- job_words %>% bind_tf_idf(word, id, n) %>% 
+    select(word, id, tf_idf) %>% spread(word, tf_idf)
+  # extract profile_tfidf
+  profile_tfidf = tfidf_vector[1, -1]
+  profile_tfidf <- as.numeric(profile_tfidf)
+  profile_tfidf[is.na(profile_tfidf)] <- 0
+  similarity_df <- frame_data(
+    ~id, ~similarity, 
+    "", 0)
+  for (i in 2:nrow(tfidf_vector)){
+    # make into vectors and get rid of NAs
+    vectorized = as.numeric(tfidf_vector[i, -1])
+    vectorized[is.na(vectorized)] <- 0
+    similarity_score <- lsa::cosine(vectorized, profile_tfidf)
+    similarity_df <- similarity_df %>%
+      add_row(id = tfidf_vector[[i, 1]], similarity = similarity_score[[1, 1]])
+  }
+  return(similarity_df)}
+
+profile_text_ex <- "I rarely take on jobs, but when I do I go all in!  Greetings,  I am a full-stack SEO, data-driven marketer and web developer with a Swiss degree in Marketing and Business Management, currently writing a thesis on Search Engines.   Past and present work includes SEO & SEM for Silicon Valley startups and videostreaming services with millions of *daily* visitors.  Why is my SEO successful? I am an actual developer, which separates me from SEO theorists who had never opened an IDE; and which makes me able to talk to programmers effectively.   Search engine optimisation:  ✓ On-site optimisation ✓ Off-site optimisation ✓ Link building ✓ Content outlining  Conversion optimisation:  ✓ Standards and business algorithms ✓ Goals ✓ Conversions and funnels ✓ Strategy ✓ Analytics ✓ Growth-hacking ✓ Project management ✓ Brand and reputation management  Team management:  ✓ Management of distributed teams with up to 20 people  Web development:  ✓ Ruby 1.9+ ✓ Ruby on Rails 4+ ✓ JavaScript and CoffeeScript ✓ HTML, CSS, HAML, SLIM, SASS/SCSS ✓ APIs ✓ Test-driven development (Rspec, Capybara)  It is high time for a change."
+
+sd <- cosine_similarities(profile_text_ex, jobs)
+
+jobs %>% left_join(sd) %>%
+  select(job_type, similarity)
+
+jobs <- read_csv("~/Dropbox/Final_Project_Shiny_App/jobs.csv")
+
+jobs <- jobs %>%
+  mutate(skills_fixed = (gsub("\\[|\\]|'", "", skills))) %>%
+  mutate(skills_fixed = as.list(strsplit(skills_fixed, ",")))
--- server.R
+++ server.R
@@ -1,6 +1,6 @@
 library(ggplot2)
 library(shiny)
-
+library(tidyverse)
 # make skill_matching function
 skill_match <- function(person_skills, job_skills = list()){
   if (length(job_skills) == 0){
@@ -10,20 +10,59 @@ skill_match <- function(person_skills, job_skills = list()){
   return (per_skill_have)
 }
 
+# make function for getting cosine similarities 
+cosine_similarities <- function(profile_text, jobs){
+  # add profile text to job words
+  job_words <- jobs %>%
+    select(snippet, id) %>%
+    add_row(snippet = profile_text, id = "profile_id", .before = 1) %>% 
+    unnest_tokens(word, snippet) %>% count(id, word, sort = TRUE) %>% ungroup()
+  # select words appearing less than 6 times and no stop words
+  rare_words <- job_words %>% count(word) %>% filter(nn < 6)
+  total_words <- job_words %>%  group_by(id) %>%summarize(total = sum(n))
+  job_words <- left_join(job_words, total_words)
+  data("stop_words")
+  job_words <- job_words %>% anti_join(stop_words) %>% anti_join(rare_words)
+  # get tfidf vectors
+  tfidf_vector <- job_words %>% bind_tf_idf(word, id, n) %>% 
+    select(word, id, tf_idf) %>% spread(word, tf_idf)
+  # extract profile_tfidf
+  profile_tfidf = tfidf_vector[1, -1]
+  profile_tfidf <- as.numeric(profile_tfidf)
+  profile_tfidf[is.na(profile_tfidf)] <- 0
+  similarity_df <- frame_data(
+    ~id, ~similarity, 
+    "", 0)
+  for (i in 2:nrow(tfidf_vector)){
+    # make into vectors and get rid of NAs
+    vectorized = as.numeric(tfidf_vector[i, -1])
+    vectorized[is.na(vectorized)] <- 0
+    similarity_score <- lsa::cosine(vectorized, profile_tfidf)
+    similarity_df <- similarity_df %>%
+      add_row(id = tfidf_vector[[i, 1]], similarity = similarity_score[[1, 1]])
+  }
+  return(similarity_df)}
+
 function(input, output, session) {
-  updateSelectizeInput(session, 'profile_skills', server = TRUE, choices = popular_job_skills$skills_fixed)
-  # Filter data based on selections
-  output$table <- DT::renderDataTable(DT::datatable({
+  values <- reactiveValues()
+  values$df <- data.frame()
+  observe({
+    # Filter data based on selections
     data <- jobs
     popular_job_skills <- popular_job_skills
     if (input$duration != "All") {
       data <- data[data$duration == input$duration,]
     }
-    # get skill match
+    # round numbers
     data <- data %>%
-      mutate(skill_match = skill_match(input$profiles_skills, skills_fixed))
+      mutate(feedback = round(feedback, 2))
+    # get skill match
+    profile_skills <- input$profile_skills
+    data <- data %>%
+      mutate(skill_match = map_dbl(skills_fixed, ~skill_match(profile_skills, .)))
     # delete first column
     data <- data %>% select(-1)
+    
     # only want job descriptions of certain length 
     data <- data %>%
       mutate(snippet_length = nchar(snippet)) %>%
@@ -43,7 +82,14 @@ function(input, output, session) {
     if (input$payment_verification_status != "All") {
       data <- data[data$payment_verification_status == input$payment_verification_status,]
     }
-    data
-  }))
-  
+    data <- data %>% 
+      select(title, snippet, workload, duration, country, feedback, payment_verification_status, 
+             skills_fixed, skill_match) %>%
+      mutate(snippet = paste0(str_sub(snippet, 1, 300), "..."))
+    values$df <- data
+  })
+  output$table <- DT::renderDataTable(DT::datatable(values$df, colnames = c("Title", "Job Description Excerpt", "Workload", "Duration", 
+                                                                            "Country", "Average Feedback", "Payment Verified", 
+                                                                            "Skills", "Skill Match")))
 }
+
